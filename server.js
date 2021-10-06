8fb6fca6466424cb67290e01ab55b1eef81b213b
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-10-06 13:20:30 -0400

    setup server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose")
+
+
